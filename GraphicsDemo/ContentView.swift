859b465a04916f72239896b37a37dfa0d34a8f60
Ahmet Goksu <ahmet@goksu.in>
2024-06-11 17:02:42 +0300

    Initial Commit

---
+++ GraphicsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GraphicsDemo
+//
+//  Created by Ahmet Göksu on 11.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GraphicsDemo/GraphicsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsDemoApp.swift
+//  GraphicsDemo
+//
+//  Created by Ahmet Göksu on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
